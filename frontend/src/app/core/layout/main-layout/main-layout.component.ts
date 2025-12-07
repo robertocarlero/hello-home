77ae92cdcfac21800f33e49b37569cb737429ddb
Roberto Carlos Quintero <robertocarlero@gmail.com>
2025-12-07 13:05:53 -0400

    feat(frontend): :sparkles: Integrate toast notifications

--- frontend/src/app/core/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@core/layout/header/header.component';
+import { ZardToastComponent } from '@shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-main-layout',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ZardToastComponent],
   templateUrl: './main-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
