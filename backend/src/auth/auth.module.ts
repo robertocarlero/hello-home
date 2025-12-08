2bd80828816cdda73d3cc805b013210c8555d875
Roberto Carlos Quintero <robertocarlero@gmail.com>
2025-12-08 15:34:45 -0400

    feat(backend): :sparkles: Update JWT token expiration from 15 minutes to 24 hours

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('jwt.secret') || 'secretKey',
-        signOptions: { expiresIn: '15m' },
+        signOptions: { expiresIn: '24h' },
       }),
       inject: [ConfigService],
     }),
